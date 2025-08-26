edba38825f4767a236d4f607acbfb50f1bacbeb1
Dwayne Dreakford <dwayne.dreakford@digital.ai>
2025-08-26 12:41:54 -0400

    Update CT endpoint.

--- run_tests_local.sh
+++ run_tests_local.sh
@@ -2,7 +2,7 @@
 #
 # Run sample tests locally via Docker
 
-export CT_URL="https://cloud.testing.com"
+export CT_URL="https://uscloud.experitest.com"
 export CT_ACCESS_KEY="your_access_key_here"
 
 # docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle gradle <gradle-task>
@@ -11,5 +11,5 @@ docker run --rm \
 -v "$PWD":/home/gradle/project \
 -e CT_URL=$CT_URL \
 -e CT_ACCESS_KEY=$CT_ACCESS_KEY \
--w /home/gradle/project gradle gradle test --tests "quickStartTests.*"
+-w /home/gradle/project gradle gradle clean test --tests "quickStartTests.*"
 
